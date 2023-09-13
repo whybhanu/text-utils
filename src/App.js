35f91f6ec05d9b99e8f8380dab4ee07482286d74
Bhanupratap Singh <bhanupratapsingh@Bhanuprataps-MacBook-Air.local>
2023-09-13 22:15:20 +0530

    previous commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,50 @@
-import logo from './logo.svg';
 import './App.css';
+import Navbar from './components/Navbar';
+import TextForm from './components/TextForm';
+// import About from './components/About';
+import Alert from './components/Alert';
+import React, {useState} from 'react'; 
 
 function App() {
+  const [mode, setMode] = useState("light")
+  const toggleMode = ()=>{
+    if (mode === "light"){
+      setMode("dark");
+      document.body.style.backgroundColor = '#042743';
+      showAlert("Dark mode has been enabled", "success");
+    }
+    else{
+      setMode("light");
+      document.body.style.backgroundColor = "white";
+      showAlert("Light mode has been enabled", "success");
+    }
+  }
+
+  const [alert, setAlert] = useState(null)
+  const showAlert = (message, type)=>{
+    setAlert({
+      msg: message,
+      type: type
+    })
+    setTimeout(() => {
+      setAlert(null);
+    }, 2000);
+  }
+
+  const [color, setColor] = useState("danger")
+  const btnColor = (color) =>{
+    setColor(color);
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <Navbar btnColor={btnColor} handleBtnClick={btnColor} title = {btnColor} mode = {mode} toggleMode= {toggleMode} aboutText = "About TextUtils"/>
+      <Alert alert={alert} type="warning"/>
+      <div className="container">
+      <TextForm color= {color} showAlert={showAlert} heading = "Enter your text here" mode = {mode}/>
+      </div> 
+      {/* <About/> */}
+    </>
   );
 }
-
 export default App;
